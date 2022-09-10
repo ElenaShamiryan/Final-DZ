c5ddd981b3d2964590832c7986c0ca414be0552d
ElenaShamiryan <elen91@list.ru>
2022-09-11 00:52:10 +0300

    вывод массива, состоящего из элементов, которые содержат <=3 символов

---
+++ ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+string[] array1 = new string[5] {"Eng", "666", "GitHub", "Ru", "HomeWork"};
+string[] array2 = new string[array1.Length];
+
+Console.WriteLine("Исходный массив");
+PrintArray(array1);
+Console.WriteLine();
+Console.WriteLine("Новый массив");
+NewArray(array1, array2);
+PrintArray(array2);
